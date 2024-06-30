0c2cffecc171c5d34355591258468d46eb413474
unknown <ballogggamer@gmail.com>
2024-06-30 16:40:22 -0300

    add countdonw animation

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -1,15 +1,37 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import "../styles/countdown.css"
 
-interface CountdownProp {
-    value: number 
+interface CountdownProps {
+    hidden: boolean,
 }
 
-const Countdown:FC<CountdownProp> = ({
-    value
+const Countdown:FC<CountdownProps> = ({
+    hidden
 }) => {
+    let [count, setCount] = useState(3)
+    let [active, setActive] = useState(hidden)
+
+    useEffect(() => {
+        if (count == 0) {
+            setCount(3)
+            return setActive(false)
+        } 
+
+        let time = setTimeout(() => {
+            setCount(--count)
+        }, 500)
+
+        return () => clearTimeout(time)
+    },[count])
+
     return (
-        <h1 className="countdown">{value}</h1>
+        <>
+            { active && 
+                <h1 className="countdown">
+                    {count}
+                </h1>
+            }
+        </>
     )
 }
 
